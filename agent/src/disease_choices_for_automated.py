57ce20a449a55a44088c45966a483a2302b44c22
nking <nichole@climbwithyourfeet.com>
2025-04-15 10:04:18 -0700

    fixed a bug in automated choice list

--- agent/src/disease_choices_for_automated.py
+++ agent/src/disease_choices_for_automated.py
@@ -3,10 +3,8 @@ def select_disease_name_randomly():
   '''
   a method for use in automated running of the Kaggle notebook
   '''
-  opts = ['heart disease', \
-   'colorectal cancer', 'breast cancer', 'stomach cancer', 'liver cancer', \
+  opts = ['heart disease', 'colorectal cancer', 'breast cancer', 'stomach cancer', 'liver cancer', \
    'pancreatic cancer', 'prostate cancer', 'lung cancer', 'ovarian cancer',\
-   'stroke', 'bronchitis', 'asthma', 'pneumonia', \
-   "alzheimer's", "parkinson's", "dementia"\
+   'stroke', 'bronchitis', 'asthma', 'pneumonia', "alzheimer's", "parkinson's", "dementia",\
    "diabetes type I", "diabetes type II"]
   return random.choice(opts)
